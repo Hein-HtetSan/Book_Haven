0308651a164432e8c52222e6b3e0f59ba67fdeeb
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-07-09 13:27:09 +0630

    1.0.0

---
+++ src/assets/js/shop.js
@@ -0,0 +1,29 @@
+const wishListBtn = document.querySelector(".item .wishlist");
+const cartBtn = document.querySelector(".item .cart");
+const heart = document.querySelector(".add-wishlist");
+const heartFill = document.querySelector(".added-wishlist");
+const cart = document.querySelector(".add-cart");
+const cartFill = document.querySelector(".added-cart");
+const categoryBtn = document.querySelector(".categ");
+const categoryFrame = document.querySelector(".cate-wrapper");
+const category_links = document.querySelectorAll(".cate-modal a");
+
+categoryBtn.onclick = () => {
+    categoryFrame.style.transform = "translateY(0)";
+}
+
+category_links.forEach(e => {
+    e.onclick = () => {
+        categoryFrame.style.transform = "translateY(-100%)";
+    }
+})
+
+wishListBtn.onclick = () => {
+    heart.classList.toggle("d-none");
+    heartFill.classList.toggle("d-none");
+}
+
+cartBtn.onclick = () => {
+    cart.classList.toggle("d-none");
+    cartFill.classList.toggle("d-none");
+}
