e3e4b281e1d2ed88cefedd4b52c09f6b035839db
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-07-21 15:15:49 +0630

    1.0.0

--- src/temp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    include_once("./php/config.php");
-    $text = "
-    What is Spare about? Spare (2023) is Prince Harrys highly anticipated memoir, which offers unprecedented insight into life as a royal. With remarkable candor, Harry reflects on his mothers death, his complex relationships with other family members, and his battles with the press.
-    ";
-    mysqli_query($con, "UPDATE book set description = '$text' where id = 4");
-
-?>
