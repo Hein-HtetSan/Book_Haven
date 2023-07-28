bbb394ed4cddb877a593a6fcd0559febc52f213e
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-07-28 08:20:10 +0630

    profile edit

--- src/template/edit_profile.php
+++ src/template/edit_profile.php
@@ -27,6 +27,7 @@
 </style>
 <body>
 
+    <!-- container  -->
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 profile d-flex align-items-center justify-content-center">
